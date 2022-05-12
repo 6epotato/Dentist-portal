1b2f96cb113d7c6dc4e54d4ac496c9b1aa81155a
6epotato <aarifurrahman93@gmail.com>
2022-05-12 14:11:54 +0600

    google button and authintication done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
 
 const Login = () => {
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
+
+
     return (
-        <div>
-            <h1>This is login</h1>
+        <div className='grid h-screen justify-items-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Login</h2>
+                    <div class="divider">OR</div>
+                    <button onClick={() => signInWithGoogle()} class="btn btn-outline">Continue with Google</button>
+                </div>
+            </div>
         </div>
     );
 };
